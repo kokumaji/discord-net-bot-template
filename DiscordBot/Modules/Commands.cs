b2c6da79e0ba08cc423f146baf168a9bdda775cf
Kokumaji <leon.wedelstedt@googlemail.com>
2020-01-30 17:46:48 +0100

    Initial Commit

---
+++ DiscordBot/Modules/Commands.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace DiscordBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("debug")]
+        public async Task DebugCommand()
+        {
+            await ReplyAsync("Your User ID is " + Context.User.Id);
+        }
+
+    }
+}
